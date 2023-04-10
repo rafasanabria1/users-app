b6c03a03a448a04f6dfc5f2a0a6c36a2f7bfd5a1
Rafael Sanabria Cruz <rafasanabria1@gmail.com>
2023-04-10 13:21:48 +0200

    Inicio del proyecto con ruta de componente login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+export default function Login () {
+  return (
+    <h1 className='bg-red'>Login</h1>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-
+import './globals.css'
 export default function Home () {
   return (
-    <h1 className='text-3xl font-bold underline'>
+    <h1 className='bg-red text-3xl font-bold underline'>
       Hello world!
     </h1>
   )
